7d76a43aada011b3266b6a7d9145f754af11493a
Vishal Kumar Tanwar <153623631+VishalkumarTanwar@users.noreply.github.com>
2026-03-03 23:17:29 +0530

    Time: 0 ms (100.00%) | Memory: 64.8 MB (92.35%) - LeetSync

---
+++ BINARY_SEARCH_TREE/975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root == NULL) return 0;
+
+        if (root->val >= low && root->val <= high) {
+            return root->val 
+                 + rangeSumBST(root->left, low, high)
+                 + rangeSumBST(root->right, low, high);
+        }
+
+        // If value is smaller than low → skip left subtree
+        if (root->val < low)
+            return rangeSumBST(root->right, low, high);
+
+        // If value is greater than high → skip right subtree
+        return rangeSumBST(root->left, low, high);
+    }
+};
