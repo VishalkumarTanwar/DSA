d7eaec77f6371ca73bd1674b265e31cf2fd5624c
Vishal008 <153623631+VishalkumarTanwar@users.noreply.github.com>
2026-06-20 10:01:38 +0530

    Time: 65 ms (9.08%) | Memory: 78.2 MB (15.48%) - LeetSync

--- BINARY_SEARCH_TREE/41-first-missing-positive/first-missing-positive.cpp
+++ BINARY_SEARCH_TREE/41-first-missing-positive/first-missing-positive.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int n= nums.size();
-        for( int i=0;i<n;i++){
-            while(nums[i]>=1 && nums[i]<=n && nums[nums[i]-1]!=nums[i]){
-                swap(nums[i],nums[nums[i]-1]);
-            }
-        }
-            for (int i = 0; i < n; i++) {
-                  if (nums[i]!=i+1){
-                    return i+1;
-                  }
+        unordered_set<int> s(nums.begin(), nums.end());
 
-                        
-                                               
-            
-        }
-        return n+1;
+        int ans = 1;
+        while (s.count(ans))
+            ans++;
+
+        return ans;
     }
 };
