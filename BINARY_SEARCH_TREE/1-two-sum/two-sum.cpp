2ae97f54d38ee8660410787ddb2af7f257d0b218
Vishal008 <153623631+VishalkumarTanwar@users.noreply.github.com>
2026-06-23 10:23:15 +0530

    Time: 0 ms (100.00%) | Memory: 14.7 MB (63.31%) - LeetSync

--- BINARY_SEARCH_TREE/1-two-sum/two-sum.cpp
+++ BINARY_SEARCH_TREE/1-two-sum/two-sum.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       // pair<int,int>ans(0,0);
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target) return {i,j} ;
+       unordered_map<int,int>mpp;
+    
+    for(int i=0;i<nums.size();i++){
+         int need = target-nums[i];
+            if(mpp.find(need)!=mpp.end()){
+               return { mpp[need],i};
             }
-        }
-        return {};
+          mpp[nums[i]]=i;
+    }
+    return {};
     }
 };
