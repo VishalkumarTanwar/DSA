711ccc0ac9bf9f68dd94be31252a4aa7b3f7ed12
Vishal008 <153623631+VishalkumarTanwar@users.noreply.github.com>
2026-07-24 14:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (6.55%) - LeetSync

--- BINARY_SEARCH_TREE/88-merge-sorted-array/merge-sorted-array.cpp
+++ BINARY_SEARCH_TREE/88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int idx= m+n-1, i=m-1,j=n-1;
-        while( i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-                nums1[idx]=nums1[i];
-                idx--;
-                i--;
+
+        vector<int> arr;
+        int i = 0, j = 0;
+
+        while(i < m && j < n) {
+            if(nums1[i] <= nums2[j]) {
+                arr.push_back(nums1[i]);
+                i++;
             }
-            else{
-                nums1[idx]=nums2[j];
-                idx--;
-                j--;
+            else {
+                arr.push_back(nums2[j]);
+                j++;
             }
         }
-        while(j>=0){
-            nums1[idx--]=nums2[j--];
 
+        while(i < m) {
+            arr.push_back(nums1[i]);
+            i++;
         }
 
-        
+        while(j < n) {
+            arr.push_back(nums2[j]);
+            j++;
+        }
+
+        // Copy merged array back to nums1
+        for(int k = 0; k < m + n; k++) {
+            nums1[k] = arr[k];
+        }
     }
 };
